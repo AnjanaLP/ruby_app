53ae79bd5433a5864d71ed51620860254b84acdf
AnjanaLP <anjana.lpn@gmail.com>
2022-05-28 23:21:12 +0100

    dry up tests using after(:each) block

--- spec/unit/parser_spec.rb
+++ spec/unit/parser_spec.rb
@@ -6,15 +6,19 @@ describe Parser do
   let(:view)          { double :view }
   let(:file_path)     { "./spec/fixtures/files/test_webserver.log" }
 
-  before(:each) { parser.convert_file }
-
   describe '#convert_file' do
+    after(:each) { parser.convert_file }
+
+    it 'calls the foreach method on the File class and passes it the file path' do
+      expect(File).to receive(:foreach).with(file_path)
+    end
+
     it 'creates a new view object for each line of the file' do
-      expect(view_class).to have_received(:new).exactly(10).times
+      expect(view_class).to receive(:new).exactly(10).times
     end
 
     it 'passes the url and ip address to the new view object' do
-      expect(view_class).to have_received(:new).with("/contact", "184.123.665.067").twice
+      expect(view_class).to receive(:new).with("/contact", "184.123.665.067").twice
     end
   end
 
@@ -22,6 +26,7 @@ describe Parser do
     it 'returns a list of webpages with thier view count in descending view count order' do
       urls = ['/help', '/home', '/about', '/contact', '/home', '/home', '/home', '/about', '/about', '/contact']
       allow(view).to receive(:url).and_return(*urls)
+      parser.convert_file
       output = "All page views (descending):\n/home 4 views\n/about 3 views\n/contact 2 views\n/help 1 view\n\n"
       expect(parser.list_all_views).to eq output
     end
@@ -31,6 +36,7 @@ describe Parser do
     it 'returns a list of webpages with unique view count' do
       allow(view).to receive(:url).and_return('/help')
       allow(view).to receive(:ip_address).and_return('1234', '1321', '1234', '1234', '1234')
+      parser.convert_file
       output = "Unique page views (descending):\n/help 2 unique views"
       expect(parser.list_unique_views).to eq output
     end
